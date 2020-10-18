529b51dd5a76762d5045997cc5f7934952de1224
911harm <911harm@gmail.com>
2020-10-18 15:33:27 -0500

    Updates

---
+++ precache-manifest.8d8d539f3c44f65e679bbcb5d6600e30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5699b28228ea47931387ace0c5ebdf60",
+    "url": "/react-redux-example/index.html"
+  },
+  {
+    "revision": "59afebd903e0ca4cce7d",
+    "url": "/react-redux-example/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e4955fd2700c37dc6f14",
+    "url": "/react-redux-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59afebd903e0ca4cce7d",
+    "url": "/react-redux-example/static/js/2.4a5a34a2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-redux-example/static/js/2.4a5a34a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4955fd2700c37dc6f14",
+    "url": "/react-redux-example/static/js/main.19255c63.chunk.js"
+  },
+  {
+    "revision": "e73a1233ec8efc6eb921",
+    "url": "/react-redux-example/static/js/runtime-main.1ad01056.js"
+  }
+]);
